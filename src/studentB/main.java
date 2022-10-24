252d9610f22d00de152dc316f14d6003b47fbe2d
ipo5069@naver.com <ipo5069@naver.com>
2022-10-24 16:35:07 +0900

    Main.java 수정1

---
+++ src/studentB/main.java
@@ -0,0 +1,11 @@
+package studentB;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "Hello world";
+		System.out.println(s);
+	}
+
+}
